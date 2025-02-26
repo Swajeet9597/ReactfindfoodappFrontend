d658e6dd899093912403413318d5339028de37cc
Swajeet9597 <swajeetdharmadhikari@gmail.com>
2025-02-26 17:24:42 +0530

    Add message for mobile user

--- src/pages/LoginPage/LoginPg.jsx
+++ src/pages/LoginPage/LoginPg.jsx
@@ -1,13 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import "./LoginPg.css"
 import Login from '../../components/Login/Login';
 import LoadingPage from '../../components/LoadPage/Loading';
 const LoginPg = () => {
   const [load,setLoad] = useState(false)
+
+  const getScreen = () =>{ 
+    return{
+      width:window.innerWidth,
+    };
+  };
+
+  const [screeSize, setScreenSize] = useState(getScreen());
+
+  useEffect(()=>{
+    const Screen = () =>{
+        setScreenSize(getScreen());
+    };
+    window.addEventListener('resize', Screen);
+
+  },[]);
+
+
   return (
     <div className="loginPage">
-        <Login setLoad={setLoad}/>
-        {load ? <LoadingPage/> : <></>}
+      {screeSize.width < 850 ? <div style={{color:"white",fontSize:"2rem", height:"100vh", display:"flex", justifyContent:"center", alignItems:"center", flexWrap:"nowrap",textAlign:"center"}}> This website is designed to work best on PCs and laptops for an enhanced experience. </div> :
+          <>
+              <Login setLoad={setLoad}/>
+              {load ? <LoadingPage/> : <></>}
+          </>
+      }
     </div>
   );
 }
