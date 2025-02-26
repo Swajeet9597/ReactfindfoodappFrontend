ebbeab5ab33bc7d46ea7fedf6d67abce621d37df
Swajeet9597 <swajeetdharmadhikari@gmail.com>
2025-02-26 16:44:24 +0530

    changes in backend url

--- src/helper.js
+++ src/helper.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://react-find-food-app-backend.vercel.app"
-export const BASE_URL = "https://reactfindfoodappbackend.onrender.com"
+export const BASE_URL = "https://react-find-food-app-backend.vercel.app"
+// export const BASE_URL = "https://reactfindfoodappbackend.onrender.com"
