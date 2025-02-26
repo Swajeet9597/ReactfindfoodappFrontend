fdbdaf5dc7ec7cea1064adf91d7237425c576a45
Swajeet9597 <swajeetdharmadhikari@gmail.com>
2025-02-26 17:27:32 +0530

    message in reg page for mobile user

--- src/pages/RegisterPage/RegisterPg.jsx
+++ src/pages/RegisterPage/RegisterPg.jsx
@@ -1,13 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import "./RegisterPg.css"
 import Register from '../../components/Register/Register';
 import LoadingPage from '../../components/LoadPage/Loading';
 const RegisterPg = () => {
   const [load,setLoad] = useState(false)
+
+  const getScreen = () =>{ 
+    return{
+      width:window.innerWidth,
+    };
+  };
+
+  const [screeSize, setScreenSize] = useState(getScreen());
+
+  useEffect(()=>{
+    const Screen = () =>{
+        setScreenSize(getScreen());
+    };
+    window.addEventListener('resize', Screen);
+
+  },[]);
+
   return (
     <div className="Regpage">
-        <Register setLoad={setLoad}/>
-        {load ? <LoadingPage/> : <></>}
+       <div className="loginPage">
+      {screeSize.width < 850 ? <div style={{color:"white",fontSize:"2rem", height:"100vh", display:"flex", justifyContent:"center", alignItems:"center", flexWrap:"nowrap",textAlign:"center"}}> This website is designed to work best on PCs and laptops for an enhanced experience. </div> :
+           <>
+              <Register setLoad={setLoad}/>
+              {load ? <LoadingPage/> : <></>}
+           </>  
+      }
     </div>
   );
 }
